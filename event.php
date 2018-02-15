1b0428de2a3e656e69a427d4badeed2c76434ff9
justarya <just1arya@gmail.com>
2018-02-15 10:35:28 +0700

    Front End update
    Front update
    - Asset
    - Profile, Event, Info, Galeri, Kontak
    - Resonsive Update

---
+++ event.php
@@ -0,0 +1,44 @@
+<?php require_once('view/header.php'); ?>
+
+<div id="event">
+  <div class="event-item-container">
+    <div class="title">
+      <h1>Event</h1>
+    </div>
+  </div>
+  <!-- baris ke 1 -->
+  <div class="event-item-container">
+    <div class="event-item">
+      <img src="assets\soccer-ball-variant.svg" alt="Futsal">
+    </div>
+    <div class="event-item">
+      <img src="assets\basketball-ball-variant.svg" alt="Basket">
+    </div>
+    <div class="event-item">
+      <img src="assets\floorball-icon.svg" alt="Floorball">
+    </div>
+    <!-- baris ke 2 -->
+    <div class="event-item">
+      <img src="assets\saman-icon.svg" alt="Tari Saman">
+    </div>
+    <div class="event-item">
+      <img src="assets\pmr-icon.svg" alt="PMR">
+    </div>
+    <div class="event-item">
+      <img src="assets\band.svg" alt="Band">
+    <!-- baris ke 2 -->
+    </div>
+    <div class="event-item">
+      <img src="assets\microphone.svg" alt="Solo Vocal">
+      <i class=""></i>
+    </div>
+    <div class="event-item">
+      <i class=""></i>
+    </div>
+    <div class="event-item">
+      <i class=""></i>
+    </div>
+  </div>
+</div>
+
+<?php require_once('view/footer.php'); ?>
--- view/header.php
+++ view/header.php
@@ -2,8 +2,17 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Satelite 19th</title>
+    <link rel="stylesheet" href="css/profile.css">
+    <link rel="stylesheet" href="css/info.css">
+    <link rel="stylesheet" href="css/event.css">
+    <link rel="stylesheet" href="css/galeri.css">
+    <link rel="stylesheet" href="css/kontak.css">
     <link rel="stylesheet" href="css/style.css">
+
+    <!-- fontawesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   </head>
   <body>
     <header>
@@ -12,11 +21,11 @@
           <div class="sponsor-item yayasan-telkom">
             <img src="assets/telkom-indonesia.png" alt="Telkom Indonesia">
           </div>
-          <div class="sponsor-item telkom-school">
-            <img src="assets/telkom-school.png" alt="Telkom School">
-          </div>
           <div class="sponsor-item telkom-indonesia">
             <img src="assets/yayasan-pendidikan-telkom.png" alt="Yayasan Pendidikan Telkom">
+          </div>
+          <div class="sponsor-item telkom-school">
+            <img src="assets/telkom-school.png" alt="Telkom School">
           </div>
         </div>
         <div class="main">
@@ -24,19 +33,20 @@
             <h1>SATELITE 19th</h1>
           </div>
           <div class="button">
-            <a href="#">Daftar Yuk!</a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSeGH9TSHcIHQHPZijyVTQqn-oJk_p-g0C0AT-1_B1mTvVHFkQ/viewform?usp=sf_link">Daftar Yuk!</a>
           </div>
         </div>
       </div>
     </header>
     <nav>
-      <div class="container">
+      <div class="container-nav">
         <ul>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Info</a></li>
-          <li><a href="#">Event</a></li>
-          <li><a href="#">Galeri</a></li>
-          <li><a href="#">Kontak</a></li>
+          <?php $class = "class='active'"; ?>
+          <li><a <?php if(basename($_SERVER["PHP_SELF"], ".php") == "index" || basename($_SERVER["PHP_SELF"], ".php") == "profile"){ echo $class;}?> href="profile.php">Profile</a></li>
+          <li><a <?php if(basename($_SERVER["PHP_SELF"], ".php") == "info"){ echo $class;}?> href="info.php">Info</a></li>
+          <li><a <?php if(basename($_SERVER["PHP_SELF"], ".php") == "event"){ echo $class;}?> href="event.php">Event</a></li>
+          <li><a <?php if(basename($_SERVER["PHP_SELF"], ".php") == "galeri"){ echo $class;}?> href="galeri.php">Galeri</a></li>
+          <li><a <?php if(basename($_SERVER["PHP_SELF"], ".php") == "kontak"){ echo $class;}?> href="kontak.php">Kontak</a></li>
         </ul>
       </div>
     </nav>
